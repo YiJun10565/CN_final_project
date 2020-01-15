f4caea937cc86d7e21aa8f3bef4fb1f5858b65e7
b06705059 <b06705059@linux2.csie.ntu.edu.tw>
2020-01-15 21:51:55 +0800

    asdzx5812

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import socket
 import select
 import types
-first = 0
+
 def accept_wrapper(s):
+    #only deal with accept client
     conn, addr = s.accept()
     print('accept connection from', addr)
     conn.setblocking(False)
@@ -16,35 +17,45 @@ def do_service(recv_data):
     elif recv_data == 'Sign up':
         print('ad')
     # not done yet
+    #
+    #
+    #
+    #
+
 
 def service_connection(s):
-    # if there are some contexts coming
+    # if client send a request. If the data of the request is none, close this client's fd
     data = s.recv(1024)
     if data:
         do_service(data)
-    if not data:
+    else:
         print('closing connection to', addr_list[s.fileno()])
         addr_list[s.fileno()] = ''
         readset.remove(s)
         s.close()
 
-
+#-------Initialize all variable-------
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 1234
 addr_list = []
 for i in range(15):
     addr_list.append('')
+    ##create server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen(5)
 addr_list[server.fileno()] = HOST
+    ##create select set
 readset = [server]
 writeset = []
 exceptionset = []
 server.setblocking(False)
-#sel.register(server, selectors.EVENT_READ, data=None)
+#-------------------------------------
+
 print(f'the server is listening at {HOST}:{PORT}')
 print('waiting for connection...')
+
+
 """conn, addr = server.accept()
 
 print(f'connect success addr={addr}')
@@ -60,9 +71,9 @@ while True:
 while True:
     readable,writable,exceptionable = select.select(readset,writeset,exceptionset,0)
     for s in readable:
-        if s is server:
-            accept_wrapper(s)
-        else:    
+        if s is server: # server get a request to create a fd for client
+            accept_wrapper(s) 
+        else:           # server serve a client reqeust
             service_connection(s)
 
 
