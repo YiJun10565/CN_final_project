1d221a5a80ad7280e358720b0c52f0adcb9a5dcc
YiJun10565 <isaachuang1425@gmail.com>
2020-01-16 16:12:47 +0800

    Update Server
    Let our server do most of the things.
    Sign in/up, Login interface

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import select
 import types
 import os
 import base64
+import csv
+
 
 def accept_wrapper(s):
     #only deal with accept client
@@ -10,81 +12,114 @@ def accept_wrapper(s):
     print('accept connection from', addr)
     conn.setblocking(False)
     addr_list[conn.fileno()] = addr
+    State_list[conn.fileno()] = "Login Interface"
     readset.append(conn)
 
-def sign_up_service(data):
-    #get the account and password form client sending
-    account = data[1]
-    password = data[2]
-    #open the passwd file to check
-    path = 'passwd'
-    f = open(path,'r+')
-    passwd_list = f.read()
-    passwd_list = passwd_list.split(":")
-    passwd_list = passwd_list[:-1]# this line is to remove the last '\n'
-    #create a string that contains currently context of passwd file
-    data_back_passwd = ''
-    for j in passwd_list:
-        data_back_passwd += str(j) + ":"
-    
-    if account in passwd_list:
-        send_data = 'NAK'
-        s.send(send_data.encode())
-    else:
-        send_data = 'ACK'
-        s.send(send_data.encode())
-        password = base64.b64encode(password.encode())
-        password = password.decode()
-        #if sign up success, append the account and encrypted password to passwd file
-        data_back_passwd += account + ":" + password + ":"
-        print('register success')
-    #write all context back to passwd file
-    f.seek(0)
-    f.write(data_back_passwd)
-    f.close()
-
-def sign_in_service(data):
+def sign_up_service(fileno, data):
     #get the account and password from client sending
-    account = data[1]
-    password = data[2]
-    #open the passwd file to check
-    path = 'passwd'
-    f = open(path, 'r')
-    passwd_list = f.read()
-    passwd_list = passwd_list.split(":")
-    passwd_list = passwd_list[:-1] # this line is to remove the last '\n'
-    if account in passwd_list:
-        #our passwd data structure is [account id:encrypted password:]
-        #so the next index of the account id is its password
-        idx = passwd_list.index(account)
-        pwd_from_file = passwd_list[idx + 1]
-        pwd_from_file = base64.b64decode(pwd_from_file.encode())
-        pwd_from_file = pwd_from_file.decode()
-        #check the password
-        #if true send ACK to client
-        #else send NAK to client 
-        if pwd_from_file == password:
-            send_data = 'ACK'
-            s.send(send_data.encode())
+    if(sub_State_list[fileno] == "Enter Account"):
+        if data in Account_Dict:
+            send_data = "Account has already existed:"
         else:
-            send_data = 'NAK'
-            s.send(send_data.encode())
-    else:
-        send_data = 'NAK'
-        s.send(send_data.encode())
-    f.close()
+            acc_tmp[fileno] = data
+            sub_State_list[fileno] = "Enter Password"
+            send_data = "Please enter the password you want:"
 
-def do_service(recv_data):
+    elif(sub_State_list[fileno] == "Enter Password"):
+        tmp_pwd[fileno] = data
+        send_data = "Please enter the password again:"
+        sub_State_list[fileno] = "Enter Password again"
+    
+    elif(sub_State_list[fileno] == "Enter Password again"):
+        if data == tmp_pwd[fileno]:
+            encrypted_pwd = base64.b64encode(data.encode()).decode()
+            Account_Dict.update( {tmp_acc[fileno]:encrypted_pwd})
+            send_data = ""
+        else:
+            send_data = "Wrong\nPlease enter the password you want:"
+            sub_State_list[fileno] = "Enter Account"
+            tmp_pwd[fileno] = ""
+            tmp_acc[fileno] = ""
+    s.send(send_data.encode())
+
+    
+    
+    #create a string that contains currently context of passwd file
+    #data_back_passwd = ''
+    #for j in passwd_list:
+    #    data_back_passwd += str(j) + ":"
+    #if account in passwd_list:
+    #    send_data = 'NAK'
+    #    s.send(send_data.encode())
+    #else:
+    #    send_data = 'ACK'
+    #    s.send(send_data.encode())
+    #    password = base64.b64encode(password.encode())
+    #    password = password.decode()
+    #    #if sign up success, append the account and encrypted password to passwd file
+    #    data_back_passwd += account + ":" + password + ":"
+    #    print('register success')
+    ##write all context back to passwd file
+    #f.seek(0)
+    #f.write(data_back_passwd)
+    #f.close()
+
+def sign_in_service(fileno, data):
+
+    #get the account and password from client sending
+    #if data == '(Exit)':
+        
+        # Back to login interface
+        # Communicate_Exit(fileno)
+
+    if sub_State_list[fileno] == "Enter Account":
+        if data in Account_Dict:
+            tmp_acc[fileno] = data
+            sub_State_list[fileno] = "Enter Password"
+            send_data = "Enter Password:"
+        else:
+            send_data = "Account not exists, please enter a valid account"
+
+    elif sub_State_list[fileno] == "Enter Password":
+        if base64.b64encode(data.encode()).decode() == Account_Dict[tmp_acc[fileno]] :
+            send_data = "Login successfully " + tmp_acc[fileno]
+            State_list[fileno] = "Choose person"
+            sub_State_list[fileno] = "Unuse"
+        else :
+            send_data = "Wrong password, please enter again."
+    send_data += "\nYou can enter '(Exit)' to exit whenever you want."
+
+    s.send(send_data.encode())
+    
+def Login_service(fileno, data):
+    if(data == "Sign in"):
+        send_data = "Please enter your account:"
+        State_list[fileno] = "Sign in"
+        sub_State_list[fileno] = "Enter Account"
+    elif(data == "Sign up"):
+        send_data = "Please enter the account you want:"
+        State_list[fileno] = "Sign up"
+        sub_State_list[fileno] = "Enter Account"
+    else :
+        send_data = "Unknown command, please try again\n"
+        send_data += "Enter 'Sign in' to sign in, 'Sign up' to sign up."
+    s.send(send_data(encode()))
+
+def do_service(fileno, recv_data):
     recv_data = recv_data.decode()
-    data = recv_data.split(":")
-    if data[0] == 'Sign in':
-        sign_in_service(data) 
-    elif data[0] == 'Sign up':
-        sign_up_service(data)
+    if(State_list[fileno] == "Login Interface"):
+        Login_service(fileno, recv_data)
+    elif(State_list[fileno] == "Sign in"):
+        sign_in_service(fileno, recv_data)        
+    elif(State_list[fileno] == "Sign up"):
+        sign_up_service(fileno, recv_data)
+                
+    # data = recv_data.split(":")
+    # if data[0] == 'Sign in':
+    #     sign_in_service(data) 
+    # elif data[0] == 'Sign up':
+    #     sign_up_service(data)
     # not done yet
-    #
-    #
-    #
     #
 
 
@@ -92,41 +127,71 @@ def service_connection(s):
     # if client send a request. If the data of the request is none, close this client's fd
     data = s.recv(1024)
     if data:
-        do_service(data)
+        do_service(s.fileno, data)
     else:
         print('closing connection to', addr_list[s.fileno()])
         addr_list[s.fileno()] = ''
+        State_list[s.fileno()] = "Unuse"
+        sub_State_list[s.fileno()] = "Unuse"
         readset.remove(s)
         s.close()
 
-#-------Initialize all variable-------
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 1234
-addr_list = []
-for i in range(15):
-    addr_list.append('')
-    ##create server socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(5)
-addr_list[server.fileno()] = HOST
-    ##create select set
-readset = [server]
-writeset = []
-exceptionset = []
-server.setblocking(False)
-#-------------------------------------
+
+if __name__ == "__main__":
+    #-------Initialize all variable-------
+    HOST = socket.gethostbyname(socket.gethostname())
+    PORT = 1234
+    
+    # Potential State_list = [ "Unuse", "Login Interface", "Sign in", "Sign up", "Choose person", "Communicating"]
+    # Potential sub_State_list = [ "Unuse", "Disconnected", "Enter Account", "Enter Password", "Enter Password again", "Receive Offline message", "Start talking"]
+    # a fileno -> a state and a substate
+    addr_list = []
+    State_list = []
+    sub_State_list = []
+    # these 2 lists are for the server to store msg
+    # due to there will be 2 different pkg for the 2 msg
+    tmp_acc = []
+    tmp_pwd = []
+    for i in range(15):
+        addr_list.append('')
+        tmp_acc.append('')
+        tmp_pwd.append('')
+        State_list.append("Unuse")
+        sub_State_list.append("Unuse")
+        ##create server socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(5)
+    addr_list[server.fileno()] = HOST
+        ##create select set
+    readset = [server]
+    writeset = []
+    exceptionset = []
+    server.setblocking(False)
+
+    
+    Account_Dict = {}
+    # read Account file in csv 
+    with open("./Account.csv", newline='') as csvfile:
+        # rows is a 2-dim list, [ [acnt0, pswd0],[acnt1, pswd1], ... ]
+        rows = csv.reader(csvfile)    
+        for row in rows:
+            # Save accounts info into a local dictionary
+            Account_Dict.update({row[0]: row[1]})
+            # print(row)
+
+    #-------------------------------------
 
 
-print(f'the server is listening at {HOST}:{PORT}')
-print('waiting for connection...')
+    print(f'the server is listening at {HOST}:{PORT}')
+    print('waiting for connection...')
 
-while True:
-    readable,writable,exceptionable = select.select(readset,writeset,exceptionset,0)
-    for s in readable:
-        if s is server: # server get a request to create a fd for client
-            accept_wrapper(s) 
-        else:           # server serve a client reqeust
-            service_connection(s)
+    while True:
+        readable,writable,exceptionable = select.select(readset,writeset,exceptionset,0)
+        for s in readable:
+            if s is server: # server get a request to create a fd for client
+                accept_wrapper(s) 
+            else:           # server serve a client reqeust
+                service_connection(s)
 
 
