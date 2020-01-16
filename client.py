562a2c53b72613c9870cfd15b5bd8892b7dad28e
b06705059 <b06705059@linux2.csie.ntu.edu.tw>
2020-01-16 16:31:38 +0800

    213

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
-import selectors
+import select
 import sys
 import getpass
-import types
+import typesa
 
 #control the client status for command uses
 state = 'INITIAL'
-
+account = ''
 def do_service(message):
     global state #get global variable state
     
@@ -44,7 +44,24 @@ def do_service(message):
             state = 'Sign in'
         else:
             print('Account id/password wrong!!')
-        
+def serve(s):
+    global state
+    global account
+    data = s.recv(1024)
+    data = data.decode()
+    print(data)
+    if 'Login successfully' in data:
+        state = 'Login'
+        data = data.split(" ")
+        account = data[2] + " : "
+    sys.stdin.flush()
+    if state == 'INITIAL':
+        if 'Password' in data;
+            inp = getpass.getpass('')
+        else:
+            inp = input('')
+    else:
+        inp = input(account)
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 1234
@@ -52,7 +69,9 @@ PORT = 1234
 server_addr = (HOST, PORT)
 print('starting connection to', server_addr)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+readset = [server]
+writeset = []
+exceptionset = []
 #check the connection whehter success
 if sock.connect_ex(server_addr) != 0:
     fail = True 
@@ -62,5 +81,8 @@ else:
     print('Success connect!')
     #begin to interact with server
     while True:
-        message = input('please input command:')
-        do_service(message)
+        readable, writable, exceptionable = select.select(readset, writeset, exceptionset, 0)
+        for s in readable:
+            #message = input('please input command:')
+            #do_service(message)
+            serve(s)
